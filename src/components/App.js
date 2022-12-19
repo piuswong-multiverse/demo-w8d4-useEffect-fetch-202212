affdc8ecd0e2313ca805d6108e92448527ab963d
piuswong-multiverse <111539998+piuswong-multiverse@users.noreply.github.com>
2022-12-19 11:16:40 -0600

    Get jokes data and display some of it

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TitleCard from './TitleCard';
 import Jokes from './Jokes';
+import './styles.css'
 
 function App(){
     return(
--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Jokes(){
+    // define important states
+    [jokes, setJokes] = useState([]); // jokes to display
+
+    // get jokes data first
+    const getJokes = async () => {
+        const response = await fetch('https://v2.jokeapi.dev/joke/Programming?amount=10&safe-mode');
+        const data = await response.json();
+        console.log(data.jokes); // debugging only
+        setJokes(data.jokes); // remember the jokes to display
+    }
+
+    // get jokes upon first loading this component
+    useEffect(() => {
+        // this is what we're going to do when triggered
+        getJokes();
+    }, []); // [] ==> trigger this when first loading
+
+    // display the jokes
     return(
-        <div classNames="jokes">
-            JOKES GO HERE
+        <div className="jokes">
+            {
+                jokes.map( (joke) => {
+                    return(
+                        <div className="joke">
+                            { joke.joke }
+                        </div>
+                    )
+                } )
+            }
         </div>
     )
 }
